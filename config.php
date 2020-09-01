520dc3feadd8e2f5b9238a99e7a14f5e1b8e86ff
Vilius Gecas <55325682+viliusgec@users.noreply.github.com>
2020-09-01 16:23:21 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 class config {
-	const DB_SERVER    = 'localhost';
-	const DB_NAME      = 'nfqsite';
-	const DB_USERNAME  = 'root';
-	const DB_PASSWORD  = '';
+	const DB_SERVER    = 'j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+	const DB_NAME      = '	rzgs242cehxokld6';
+	const DB_USERNAME  = 'x8u1sevynj2z5a3t';
+	const DB_PASSWORD  = 'wxi6ekban37o3f2v';
 	const DB_PREFIX	   = ''; 
 	const NUMBER_OF_ROWS_IN_PAGE = 10;
 }
